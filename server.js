eea0c10c1f10a084063b1f87d4c3101053210af7
Himanshu Vyas <hvyas7459@gmail.com>
2026-04-09 17:20:28 +0530

    fix cors

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 5000
 connectDB()
 
 app.use(cors({
-  origin: "https://utsav-ai.vercel.app/",
+  origin: "https://utsav-ai.vercel.app",
   credentials: true
 }));
 
