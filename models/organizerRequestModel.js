f12b7de71cddfd707043b80889dc0e80e9c34bf2
Himanshu Vyas <hvyas7459@gmail.com>
2026-04-19 17:54:52 +0530

    organizer fixed

---
+++ models/organizerRequestModel.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const organizerRequestSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  status: {
+    type: String,
+    default: "pending",
+  },
+}, { timestamps: true });
+
+export default mongoose.model("OrganizerRequest", organizerRequestSchema);
+
+
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+
+// const organizerRequestSchema = new mongoose.Schema({
+//   user: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: "User"
+//   },
+//   status: {
+//     type: String,
+//     enum: ["pending", "approved", "rejected"],
+//     default: "pending"
+//   }
+// }, { timestamps: true });
+
+// export default mongoose.model("OrganizerRequest", organizerRequestSchema);
